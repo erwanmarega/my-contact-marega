8b0725784445d4de463a0342423bf1fddf7bd3f3
Erwan Marega <maregaerwan@gmail.com>
2025-09-16 10:13:59 +0200

    réajustement de la navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="flex ">
       <nav className="flex flex-col w-14 h-screen bg-gray-800 text-white shadow-lg">
-        <div className="flex flex-col items-center mt-4 space-y-4">
+        <div className="flex flex-col items-center mt-4 space-y-4 flex-grow">
           <a href="/dashboard" className="hover:bg-gray-700 p-3 rounded-lg">
             <HomeIcon className="w-6 h-6" />
           </a>
@@ -15,13 +15,13 @@ const Navbar = () => {
           <a href="#settings" className="hover:bg-gray-700 p-3 rounded-lg">
             <Search className="w-6 h-6" />
           </a>
-            <a href="/logout" className="hover:bg-gray-700 p-3 rounded-lg mt-auto mb-4">
-                <LogOut className="w-6 h-6" />
-            </a>
+        </div>
+        <div className="mt-auto mb-4 ml-auto mr-auto">
+          <a href="/logout" className="hover:bg-gray-700 p-3 rounded-lg">
+            <LogOut className="w-6 h-6 " />
+          </a>
         </div>
       </nav>
-
-      
     </div>
   );
 };
